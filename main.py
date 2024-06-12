Fixed off-by one indexing error in Markov model

main.py
@@ -3,17 +3,21 @@ import trading.models.markov as markov
 
 from torch.utils.data import DataLoader
 import torch.nn as nn
+import numpy as np
 
 NIFTY = YTicker("^NSEI")
 
-data = models.TickerUpDownDataset(NIFTY, period=periods.years5, lookahead=2, min_lookback=1, max_lookback=1,
+data = models.TickerUpDownDataset(NIFTY, period=periods.years5, lookahead=1, min_lookback=1, max_lookback=1,
                                   x=("Close", "Prev Close"))
-data["Up"] = (data["Close"] > data["Prev Close"]).astype("float32")
 
-dataloader = DataLoader(data)
+args = []
+for t in np.arange(0.9, 1.1, 0.02):
+    data[f"Up{t}"] = (data["Close"] > data["Prev Close"] * t).astype("float32")
+    args.append(markov.Bool(f"Up{t}"))
+
+dataloader = DataLoader(data, batch_size=1)
 loss = nn.L1Loss()
 
-model = models.MarkovClassifier(dataloader, markov.Bool("Up"))
-# model = models.UpClassifier()
+model = models.MarkovClassifier(dataloader, *args)
 tester = models.Backtest(model, dataloader, loss)
 tester.test()

trading/models/backtesting.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import torch
 import torch.nn as nn
 
@@ -22,6 +23,9 @@ class Backtest:
         self._model.to(self._device)
         self._model.eval()
 
+        X = []
+        Y = []
+
         with torch.no_grad():
             x: torch.Tensor
             y: torch.Tensor
@@ -30,10 +34,17 @@ class Backtest:
                 x = x.to(self._device)
                 y = y.to(self._device)
 
+                X.append(x.cpu().numpy().squeeze())
+                Y.append(y.cpu().numpy()[0])
+
                 pred = self._model(x)
                 test_loss += self._loss_fn(pred, y).item()
                 correct += (pred == y).type(torch.float).sum().item()
 
+        print(pd.DataFrame({"x": X, "y": Y}))
+
         test_loss /= num_batches
-        correct /= size
-        print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+        print(f"Test Error:"
+              f"\n    Accuracy: {(100 * correct / size):>0.1f}%"
+              f"\n    Avg loss: {test_loss:>8f}"
+              f"\n    Correct: {int(correct)}/{size}")

trading/models/dataloader.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 from torch.utils.data import Dataset
 import pandas as pd
@@ -25,12 +26,14 @@ class TickerUpDownDataset(Dataset):
         self._max_lookback = max_lookback
 
         self._label = y
+        self._features = list(x)
         self._ticker = ticker
         self._data = ticker.history(period=period, interval=interval)
 
         self._set_y()
-        self._data = self._data[list(x)]
+        self._data = self._data[self._features + ["y"]]
         self._set_x()
+        print(self._data)
 
     def __len__(self):
         return len(self._x) - self._min_lookback
@@ -42,26 +45,31 @@ class TickerUpDownDataset(Dataset):
         idx += self._min_lookback
 
         if self._max_lookback is None:
-            return self._x[:idx], self._y[idx]
-        return self._x[idx - self._max_lookback:idx], self._y[idx]
+            return self._x[:idx + 1], self._y[idx]
+        return self._x[max(0, idx - self._max_lookback + 1):idx + 1], self._y[idx]
 
     def __setitem__(self, key: str, value):
+        self._features.append(key)
         self._data[key] = value
         self._set_x()
 
     def __delitem__(self, key):
+        self._features.remove(key)
         del self._data[key]
         self._set_x()
 
     def _set_x(self):
-        self._x = self._data.values
-        self._x = self._x[:-self._lookahead]
+        self._x = self._data[self._features].values
         self._x = torch.tensor(self._x, dtype=torch.float32)
 
     def _set_y(self):
         self._y = self._data[self._label]
         self._y = self._y.shift(-self._lookahead) > self._y
+
         self._y = self._y[:-self._lookahead]
+        self._data = self._data.iloc[:-self._lookahead]
+        self._data["y"] = self._y
+
         self._y = torch.tensor(self._y.values, dtype=torch.float32)
 
     @property
@@ -80,3 +88,18 @@ class TickerUpDownDataset(Dataset):
     def data(self) -> pd.DataFrame:
         return self._data
 
+    @property
+    def x(self) -> torch.Tensor:
+        return self._x
+
+    @property
+    def y(self) -> torch.Tensor:
+        return self._y
+
+    @property
+    def label(self) -> str:
+        return self._label
+
+    @property
+    def features(self) -> tuple[str, ...]:
+        return tuple(self._features)

trading/models/markov.py
@@ -57,31 +57,38 @@ class MarkovClassifier(BinaryClassifier):
         super().__init__()
         dataset = dataloader.dataset
         assert dataset.min_lookback == dataset.max_lookback == order, "Dataset lookback must equal Markov chain order"
+        self._columns = dataset.features
 
         states = [arg.as_states(dataset[arg.name]) for arg in args]
+        # states.append(pd.Series(dataset.y))
         states = pd.DataFrame(states).T
         self._states = states.drop_duplicates().reset_index(drop=True)  # unique states
 
         # Ranking Markov states
         # i.e. each unique row becomes a different number
-        ranks = pd.DataFrame({"From": states.apply(tuple, axis=1).rank(method="dense") - 1})
+        ranks = pd.DataFrame({"rank": states.apply(tuple, axis=1).rank(method="dense") - 1})
 
         rank_to_state = self._states.apply(tuple, axis=1).to_dict()
         self._state_to_rank = pd.DataFrame(index=rank_to_state.values(), data=rank_to_state.keys())
         self._rank_to_state = pd.DataFrame(index=rank_to_state.keys(), data=rank_to_state.values(),
                                            columns=[arg.name for arg in args])
 
-        # Creating the Markov transition matrix
-        ranks["To"] = ranks["From"].shift(-1)  # shift forward so 'From' transitions to 'To'
-        ranks["_"] = 1
+        # Calculate probability of 0 (down) or 1 (up) given grouping
+        ranks["y"] = dataset.y
+        print(ranks)
+        ranks = ranks.groupby("rank")
+        self._probs = ranks["y"].mean().round()  # round() interprets mean >= 0.5 as up, < 0.5 as down
+        print(self._probs)
 
-        transitions = ranks.groupby(["From", "To"]).count().unstack()
-        transitions.columns = transitions.columns.droplevel()  # make the columns a bit neater
+        # Creating the Markov transition matrix
+        # ranks["To"] = ranks["From"].shift(-1)  # shift forward so 'From' transitions to 'To'
+        # ranks["_"] = 1
+
+        # transitions = ranks.groupby(["From", "To"]).count().unstack()
+        # transitions.columns = transitions.columns.droplevel()  # make the columns a bit neater
 
         # Finding the most likely transitions
-        self._most_likely = transitions.idxmax(axis=1).astype("int")
-        self._columns = dataset.columns
-
+        # self._most_likely = transitions.idxmax(axis=1).astype("int")
         self._args = {arg: self._columns.index(arg.name) for arg in args}
 
         # print(states)
@@ -101,8 +108,7 @@ class MarkovClassifier(BinaryClassifier):
         for arg, i in self._args.items():
             states[arg.name] = tuple(arg.as_states(x[..., i].cpu().numpy()).squeeze(axis=-1))
         states = list(zip(*states.values()))
-        rank = self._state_to_rank.loc[states][0].values
 
-        next_rank = self._most_likely.loc[rank].values
-        next_state = self._rank_to_state.loc[next_rank]
-        return torch.tensor(next_state["Up"].values, device=x.device, dtype=x.dtype)
+        rank = self._state_to_rank.loc[states][0].values
+        most_likely = self._probs.loc[rank].values
+        return torch.tensor(most_likely, device=x.device, dtype=x.dtype)